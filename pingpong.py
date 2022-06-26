de5ee75303f2748d7690b101dea8b2434b92f209
Zypo124 <107806675+Zypo124@users.noreply.github.com>
2022-06-26 20:02:42 +0300

    Add files via upload

---
+++ pingpong.py
@@ -0,0 +1,76 @@
+from pygame import*
+
+class Karakter(sprite.Sprite):
+    def __init__(self, krk_image, krk_x, krk_y, krk_hiz, genislik, yukseklik):
+        super().__init__()
+        self.image = transform.scale(image.load(krk_image),(genislik,yukseklik))
+        self.hiz = krk_hiz
+
+        self.rect = self.image.get_rect()
+        self.rect.x = krk_x
+        self.rect.y = krk_y
+
+    def ciz(self):
+        pencere.blit(self.image,(self.rect.x,self.rect.y))
+
+class Oyuncu(Karakter):
+    def update_r(self):
+        basilan_tuslar = key.get_pressed()
+        if basilan_tuslar[K_UP] and self.rect.y >5:
+            self.rect.y -= self.hiz
+        if basilan_tuslar[K_DOWN] and self.rect.y < 420:
+            self.rect.y += self.hiz
+
+
+    def update_l(self):
+        basilan_tuslar = key.get_pressed()
+        if basilan_tuslar[K_w] and self.rect.y >5:
+            self.rect.y -= self.hiz
+        if basilan_tuslar[K_s] and self.rect.y < 420:
+            self.rect.y += self.hiz
+
+raket_sol = Oyuncu("racket.png",30,200,4,50,150)
+raket_sag = Oyuncu("racket.png",520,200,4,50,150)
+
+top = Karakter("tenis_ball.png",200,200,4,50,50)
+
+
+
+
+
+
+
+# oyun penceresinin oluşturulması
+pencere = display.set_mode((600,500))
+#pencere arka plan rengi RGB kod
+pencere.fill((200,255,255))
+
+
+game = True
+FPS = 60
+clock = time.Clock()
+
+# Oyun döngüsü
+while game:
+    # Eğer çarpı işaretine basılırsa pencereyi kapa
+    for e in event.get():
+        if e.type == QUIT:
+            game = False
+
+    # ekranı güncelle
+
+    raket_sag.ciz()
+    raket_sol.ciz()
+    top.ciz()
+
+    raket_sol.update_l()
+    raket_sag.update_r()
+
+
+
+
+
+
+
+    display.update()
+    clock.tick(FPS)
